47eb10400061fe30a6a65e14952f0263b0c28a57
김상훈 <rlatkdgns042@naver.com>
2024-07-08 16:50:26 +0900

    [KAN-142] feat(messaging): SMS 인증 서비스 및 Redis 연동 완료

---
+++ server/src/main/java/kr/or/kosa/cmsplusmain/domain/messaging/controller/MessagingController.java
@@ -0,0 +1,42 @@
+package kr.or.kosa.cmsplusmain.domain.messaging.controller;
+
+import kr.or.kosa.cmsplusmain.domain.messaging.dto.SmsVerifyDto;
+import kr.or.kosa.cmsplusmain.domain.messaging.service.SmsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/vendor/auth")
+public class MessagingController {
+
+    private final SmsService smsService;
+
+    // SMS 인증번호 요청
+    @PostMapping("/sms/sending")
+    public ResponseEntity<String> sendSms(@RequestBody SmsVerifyDto smsVerifyDto) {
+        try {
+            smsService.sendSmsCode(smsVerifyDto);
+            return ResponseEntity.ok("[SMS] 인증코드 전송 성공");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
+
+    // SMS 인증번호 검사
+    @PostMapping("/sms/verification")
+    public ResponseEntity<String> SmsVerification(@RequestBody SmsVerifyDto smsVerifyDto) {
+        try {
+            smsService.verifySmsCode(smsVerifyDto);
+            return ResponseEntity.ok("[SMS] 인증 성공");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
+
+}
---
+++ server/src/main/java/kr/or/kosa/cmsplusmain/domain/messaging/service/SmsServiceImpl.java
@@ -0,0 +1,39 @@
+package kr.or.kosa.cmsplusmain.domain.messaging.service;
+
+import kr.or.kosa.cmsplusmain.domain.messaging.dto.SmsVerifyDto;
+import kr.or.kosa.cmsplusmain.domain.messaging.repository.SmsRepository;
+import kr.or.kosa.cmsplusmain.util.SmsUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class SmsServiceImpl implements SmsService {
+
+    private final SmsUtil smsUtil;
+    private final SmsRepository smsRepository;
+
+    public void sendSmsCode(SmsVerifyDto smsVerifyDto){
+        String to = smsVerifyDto.getPhone();
+        int randomNumber = (int) (Math.random() * 9000) + 1000;
+        String certificationNumber = String.valueOf(randomNumber);
+        smsUtil.sendSms(to, certificationNumber);
+        smsRepository.createSmsVerification(to, certificationNumber);
+    }
+
+    public void verifySmsCode(SmsVerifyDto smsVerifyDto) {
+        if (!isVerify(smsVerifyDto)) {
+            throw new IllegalArgumentException("[SMS] 인증번호 불일치");
+        }
+        smsRepository.removeSmsCertification(smsVerifyDto.getPhone());
+    }
+
+    public boolean isVerify(SmsVerifyDto smsVerifyDto) {
+        return smsRepository.hasKey(smsVerifyDto.getPhone()) &&
+                smsRepository.getSmsVerification(smsVerifyDto.getPhone())
+                        .equals(smsVerifyDto.getSmsVerifyCode());
+    }
+
+}
