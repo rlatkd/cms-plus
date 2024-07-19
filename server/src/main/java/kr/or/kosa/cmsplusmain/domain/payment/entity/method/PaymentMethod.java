4a01ad5ffe222c828cf49032e9a115caefd74bd8
박민석 <tir2986@naver.com>
2024-07-19 11:04:47 +0900

    [KAN-218] fix(payment): enum 세부 정보 숨김

--- server/src/main/java/kr/or/kosa/cmsplusmain/domain/payment/entity/method/PaymentMethod.java
+++ server/src/main/java/kr/or/kosa/cmsplusmain/domain/payment/entity/method/PaymentMethod.java
@@ -2,6 +2,8 @@ package kr.or.kosa.cmsplusmain.domain.payment.entity.method;
 
 import java.util.Arrays;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import kr.or.kosa.cmsplusmain.domain.base.entity.BaseEnum;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -14,9 +16,9 @@ public enum PaymentMethod implements BaseEnum {
 	ACCOUNT("계좌", false, false);
 
 	private final String title;
-	@Getter
+	@Getter @JsonIgnore
 	private final Boolean canCancel;		// 결제 취소 가능 여부
-	@Getter
+	@Getter @JsonIgnore
 	private final Boolean canPayRealtime; 	// 실시간 결제 가능 여부
 
 	public static class Const {
