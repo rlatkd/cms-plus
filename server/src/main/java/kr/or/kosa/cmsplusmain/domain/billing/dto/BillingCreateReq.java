ecba3e0b75f4ed2ef7adc62283c05659d9712833
박민석 <tir2986@naver.com>
2024-07-22 16:32:14 +0900

    [KAN-205] feat(billing): 청구 생성 dto 이름 변경

--- server/src/main/java/kr/or/kosa/cmsplusmain/domain/billing/dto/BillingCreateReq.java
+++ server/src/main/java/kr/or/kosa/cmsplusmain/domain/billing/dto/BillingCreateReq.java
@@ -10,7 +10,7 @@ import lombok.Getter;
 @Getter
 public class BillingCreateReq {
 	@NotNull private BillingType billingType;					// 청구타입 (정기, 추가)
-	@NotNull private LocalDate billingDate;						// 청구 결제일
+	@NotNull private LocalDate paymentDate;						// 청구 결제일
 	@NotNull private Long contractId;							// 청구 생성 기반 계약의 ID
-	@NotNull private List<BillingProductReq> billingProducts;	// 청구 상품 목록
+	@NotNull private List<BillingProductReq> products;	// 청구 상품 목록
 }
