c42214fbc1f68abf1d49df2304f0eb1bd312f438
김상훈 <rlatkdgns042@naver.com>
2024-07-24 13:09:08 +0900

    [KAN-229] cicd(-): test

--- server-payment/src/main/java/kr/or/kosa/cmspluspayment/config/KafkaConsumerConfig.java
+++ server-payment/src/main/java/kr/or/kosa/cmspluspayment/config/KafkaConsumerConfig.java
@@ -1,44 +1,44 @@
-// package kr.or.kosa.cmspluspayment.config;
+ package kr.or.kosa.cmspluspayment.config;
 
-// import kr.or.kosa.cmspluspayment.dto.PaymentDto;
-// import org.apache.kafka.clients.consumer.ConsumerConfig;
-// import org.apache.kafka.common.serialization.StringDeserializer;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-// import org.springframework.kafka.core.ConsumerFactory;
-// import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-// import org.springframework.kafka.listener.ContainerProperties;
-// import org.springframework.kafka.support.serializer.JsonDeserializer;
-// import java.util.HashMap;
-// import java.util.Map;
+ import kr.or.kosa.cmspluspayment.dto.PaymentDto;
+ import org.apache.kafka.clients.consumer.ConsumerConfig;
+ import org.apache.kafka.common.serialization.StringDeserializer;
+ import org.springframework.context.annotation.Bean;
+ import org.springframework.context.annotation.Configuration;
+ import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+ import org.springframework.kafka.core.ConsumerFactory;
+ import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+ import org.springframework.kafka.listener.ContainerProperties;
+ import org.springframework.kafka.support.serializer.JsonDeserializer;
+ import java.util.HashMap;
+ import java.util.Map;
 
-// @Configuration
-// public class KafkaConsumerConfig {
+ @Configuration
+ public class KafkaConsumerConfig {
 
-//     // 결제데이터 메인서버에서 받음
-//     @Bean
-//     public ConcurrentKafkaListenerContainerFactory<String, PaymentDto> paymentKafkaListenerContainerFactory() {
-//         ConcurrentKafkaListenerContainerFactory<String, PaymentDto> factory = new ConcurrentKafkaListenerContainerFactory<>();
-//         factory.setConsumerFactory(paymentConsumerFactory());
-//         factory.setBatchListener(true);
-//         factory.setConcurrency(3);
-//         factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.BATCH);
-//         factory.getContainerProperties().setIdleBetweenPolls(500);
-//         return factory;
-//     }
+     // 결제데이터 메인서버에서 받음
+     @Bean
+     public ConcurrentKafkaListenerContainerFactory<String, PaymentDto> paymentKafkaListenerContainerFactory() {
+         ConcurrentKafkaListenerContainerFactory<String, PaymentDto> factory = new ConcurrentKafkaListenerContainerFactory<>();
+         factory.setConsumerFactory(paymentConsumerFactory());
+         factory.setBatchListener(true);
+         factory.setConcurrency(3);
+         factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.BATCH);
+         factory.getContainerProperties().setIdleBetweenPolls(500);
+         return factory;
+     }
 
-//     @Bean
-//     public ConsumerFactory<String, PaymentDto> paymentConsumerFactory() {
-//         Map<String, Object> props = new HashMap<>();
-//         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9094");
-//         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-//         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
-//         props.put(ConsumerConfig.GROUP_ID_CONFIG, "payment-group");
-//         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-//         props.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
-//         JsonDeserializer<PaymentDto> jsonDeserializer = new JsonDeserializer<>(PaymentDto.class, false);
-//         return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), jsonDeserializer);
-//     }
+     @Bean
+     public ConsumerFactory<String, PaymentDto> paymentConsumerFactory() {
+         Map<String, Object> props = new HashMap<>();
+         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "ec2-3-39-232-243.ap-northeast-2.compute.amazonaws.com:9094");
+         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+         props.put(ConsumerConfig.GROUP_ID_CONFIG, "payment-group");
+         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+         props.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
+         JsonDeserializer<PaymentDto> jsonDeserializer = new JsonDeserializer<>(PaymentDto.class, false);
+         return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), jsonDeserializer);
+     }
 
-// }
+ }
