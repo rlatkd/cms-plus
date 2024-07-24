436d809d31c5e6b92adb55b6b927756f16ff5646
김상훈 <rlatkdgns042@naver.com>
2024-07-24 09:13:27 +0900

    [KAN-229] cicd(-): test

--- server-messaging/src/main/java/kr/or/kosa/cmsplusmessaging/test/InfraTestController.java
+++ server-messaging/src/main/java/kr/or/kosa/cmsplusmessaging/test/InfraTestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class InfraTestController {
 
-    @GetMapping("/infra-test")
+    @GetMapping("/infra-test-msg")
     public String infraTest() {
         return "infra-test 333";
     }
