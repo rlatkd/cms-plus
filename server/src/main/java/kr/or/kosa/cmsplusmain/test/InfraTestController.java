6c07397fef506da4fb02f4e8040edd69c305553b
김상훈 <rlatkdgns042@naver.com>
2024-07-24 14:24:31 +0900

    [KAN-229] cicd(-): test

--- server/src/main/java/kr/or/kosa/cmsplusmain/test/InfraTestController.java
+++ server/src/main/java/kr/or/kosa/cmsplusmain/test/InfraTestController.java
@@ -8,7 +8,7 @@ public class InfraTestController {
 
     @GetMapping("/infra-test")
     public String infraTest() {
-        return "infra-test 1111111111231231";
+        return "infra-test 111111111111231231";
     }
 
 }
