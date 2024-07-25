c799f0903fda2972fd2c842ac66cbc617963e492
김상훈 <rlatkdgns042@naver.com>
2024-07-25 19:55:26 +0900

    [KAN-229] cicd(-): 배포 테스트

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,5 +1,5 @@
 import LoginForm from '@/components/LoginForm';
-import backgroundImgSide from '@/assets/backgroundImgSide.svg';
+import backgroundImgSide from '@/assets/backgroundImgSide.png';
 
 
 const LoginPage = () => {
