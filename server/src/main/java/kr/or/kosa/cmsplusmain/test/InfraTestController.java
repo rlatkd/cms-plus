a03a456ca15179398a5b4dbf875c36238e538f8a
김상훈 <rlatkdgns042@naver.com>
2024-07-27 22:56:50 +0900

    [KAN-265] feat(monitoring): 엘라스틱서치 연동

--- server/src/main/java/kr/or/kosa/cmsplusmain/test/InfraTestController.java
+++ server/src/main/java/kr/or/kosa/cmsplusmain/test/InfraTestController.java
@@ -1,13 +1,16 @@
 package kr.or.kosa.cmsplusmain.test;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j(topic = "monitoring-topic")
 public class InfraTestController {
 
     @GetMapping("/infra-test")
     public String infraTest() {
+        log.error("infraTest");
         return "infra-test v1";
     }
 
