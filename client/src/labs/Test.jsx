e9614fdc58f6a1e0a413192773f55484354c0416
김상훈 <rlatkdgns042@naver.com>
2024-07-29 15:31:00 +0900

    [KAN-229] cicd(-): BE 배포

--- client/src/labs/Test.jsx
+++ client/src/labs/Test.jsx
@@ -21,7 +21,7 @@ const Test = () => {
     const result = await alertComp(msg, type, title);
   };
 
-  console.log("infra test v0729");
+  console.log("infra test v0729-1");
 
   // type:
   // msg : 원하는 메세지
