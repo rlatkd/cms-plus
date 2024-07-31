78e5fa770e7a858488c4129f59f5ccffcd9cdd0b
김상훈 <rlatkdgns042@naver.com>
2024-07-31 20:31:47 +0900

    [KAN-229] cicd(-): 배포

--- server-batch/src/main/java/kr/or/kosa/cmsplusbatch/batch/dto/BillingProductQueryDto.java
+++ server-batch/src/main/java/kr/or/kosa/cmsplusbatch/batch/dto/BillingProductQueryDto.java
@@ -1,28 +1,28 @@
-package kr.or.kosa.cmsplusbatch.batch.dto;
+// package kr.or.kosa.cmsplusbatch.batch.dto;
 
-import com.querydsl.core.annotations.QueryProjection;
-import lombok.Getter;
+// import com.querydsl.core.annotations.QueryProjection;
+// import lombok.Getter;
 
-@Getter
-public class BillingProductQueryDto {
-    private final Long id;
-    private final Long productId;
-    private final String billingProductName;
-    private final Integer billingProductPrice;
-    private final Integer billingProductQuantity;
-    private final Long billingId; // 추가된 필드
+// @Getter
+// public class BillingProductQueryDto {
+//     private final Long id;
+//     private final Long productId;
+//     private final String billingProductName;
+//     private final Integer billingProductPrice;
+//     private final Integer billingProductQuantity;
+//     private final Long billingId;
 
-    @QueryProjection
-    public BillingProductQueryDto(Long id, Long productId, String billingProductName, Integer billingProductPrice, Integer billingProductQuantity, Long billingId) {
-        this.id = id;
-        this.productId = productId;
-        this.billingProductName = billingProductName;
-        this.billingProductPrice = billingProductPrice;
-        this.billingProductQuantity = billingProductQuantity;
-        this.billingId = billingId; // 초기화
-    }
+//     @QueryProjection
+//     public BillingProductQueryDto(Long id, Long productId, String billingProductName, Integer billingProductPrice, Integer billingProductQuantity, Long billingId) {
+//         this.id = id;
+//         this.productId = productId;
+//         this.billingProductName = billingProductName;
+//         this.billingProductPrice = billingProductPrice;
+//         this.billingProductQuantity = billingProductQuantity;
+//         this.billingId = billingId;
+//     }
 
-    public Long getBillingId() {
-        return billingId;
-    }
-}
+//     public Long getBillingId() {
+//         return billingId;
+//     }
+// }
--- server-batch/src/main/java/kr/or/kosa/cmsplusbatch/batch/dto/BillingQueryDto.java
+++ server-batch/src/main/java/kr/or/kosa/cmsplusbatch/batch/dto/BillingQueryDto.java
@@ -1,78 +1,78 @@
-package kr.or.kosa.cmsplusbatch.batch.dto;
+// package kr.or.kosa.cmsplusbatch.batch.dto;
 
-import com.querydsl.core.annotations.QueryProjection;
-import kr.or.kosa.cmsplusbatch.domain.billing.entity.Billing;
-import kr.or.kosa.cmsplusbatch.domain.billing.entity.BillingStatus;
-import kr.or.kosa.cmsplusbatch.domain.billing.entity.BillingType;
-import kr.or.kosa.cmsplusbatch.domain.contract.entity.Contract;
-import lombok.Getter;
-import lombok.Setter;
+// import com.querydsl.core.annotations.QueryProjection;
+// import kr.or.kosa.cmsplusbatch.domain.billing.entity.Billing;
+// import kr.or.kosa.cmsplusbatch.domain.billing.entity.BillingStatus;
+// import kr.or.kosa.cmsplusbatch.domain.billing.entity.BillingType;
+// import kr.or.kosa.cmsplusbatch.domain.contract.entity.Contract;
+// import lombok.Getter;
+// import lombok.Setter;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.List;
+// import java.time.LocalDate;
+// import java.time.LocalDateTime;
+// import java.util.List;
 
-@Getter
-@Setter
-public class BillingQueryDto {
+// @Getter
+// @Setter
+// public class BillingQueryDto {
 
-    private Long id;
-    private Long contractId;
-    private String billingType;
-    private Integer contractDay;
-    private LocalDate billingDate;
-    private String billingStatus;
-    private String invoiceMessage;
-    private LocalDateTime invoiceSendDateTime;
-    private LocalDateTime paidDateTime;
-    private Long totalBillingProductPrice;
-    private String memberName;
-    private String memberPhone;
-    private List<BillingProductQueryDto> billingProducts;
+//     private Long id;
+//     private Long contractId;
+//     private String billingType;
+//     private Integer contractDay;
+//     private LocalDate billingDate;
+//     private String billingStatus;
+//     private String invoiceMessage;
+//     private LocalDateTime invoiceSendDateTime;
+//     private LocalDateTime paidDateTime;
+//     private Long totalBillingProductPrice;
+//     private String memberName;
+//     private String memberPhone;
+//     private List<BillingProductQueryDto> billingProducts;
 
-    @QueryProjection
-    public BillingQueryDto(Long id, Long contractId, String billingType, Integer contractDay, LocalDate billingDate,
-                           String billingStatus, String invoiceMessage, LocalDateTime invoiceSendDateTime,
-                           LocalDateTime paidDateTime, String memberName, String memberPhone) {
-        this.id = id;
-        this.contractId = contractId;
-        this.billingType = billingType;
-        this.contractDay = contractDay;
-        this.billingDate = billingDate;
-        this.billingStatus = billingStatus;
-        this.invoiceMessage = invoiceMessage;
-        this.invoiceSendDateTime = invoiceSendDateTime;
-        this.paidDateTime = paidDateTime;
-        this.memberName = memberName;
-        this.memberPhone = memberPhone;
-    }
+//     @QueryProjection
+//     public BillingQueryDto(Long id, Long contractId, String billingType, Integer contractDay, LocalDate billingDate,
+//                            String billingStatus, String invoiceMessage, LocalDateTime invoiceSendDateTime,
+//                            LocalDateTime paidDateTime, String memberName, String memberPhone) {
+//         this.id = id;
+//         this.contractId = contractId;
+//         this.billingType = billingType;
+//         this.contractDay = contractDay;
+//         this.billingDate = billingDate;
+//         this.billingStatus = billingStatus;
+//         this.invoiceMessage = invoiceMessage;
+//         this.invoiceSendDateTime = invoiceSendDateTime;
+//         this.paidDateTime = paidDateTime;
+//         this.memberName = memberName;
+//         this.memberPhone = memberPhone;
+//     }
 
-    public void setBillingProducts(List<BillingProductQueryDto> billingProducts) {
-        this.billingProducts = billingProducts;
-    }
+//     public void setBillingProducts(List<BillingProductQueryDto> billingProducts) {
+//         this.billingProducts = billingProducts;
+//     }
 
-    public Billing toEntity(Contract contract) {
-        return Billing.builder()
-                .id(this.id)
-                .contract(contract)
-                .billingType(BillingType.valueOf(this.billingType))
-                .contractDay(this.contractDay)
-                .billingDate(this.billingDate)
-                .billingStatus(BillingStatus.valueOf(this.billingStatus))
-                .invoiceMessage(this.invoiceMessage)
-                .invoiceSendDateTime(this.invoiceSendDateTime)
-                .paidDateTime(this.paidDateTime)
-                .build();
-    }
+//     public Billing toEntity(Contract contract) {
+//         return Billing.builder()
+//                 .id(this.id)
+//                 .contract(contract)
+//                 .billingType(BillingType.valueOf(this.billingType))
+//                 .contractDay(this.contractDay)
+//                 .billingDate(this.billingDate)
+//                 .billingStatus(BillingStatus.valueOf(this.billingStatus))
+//                 .invoiceMessage(this.invoiceMessage)
+//                 .invoiceSendDateTime(this.invoiceSendDateTime)
+//                 .paidDateTime(this.paidDateTime)
+//                 .build();
+//     }
 
-    public void setInvoiceSent() {
-        this.billingStatus = "WAITING_PAYMENT";
-        this.invoiceSendDateTime = LocalDateTime.now();
-    }
+//     public void setInvoiceSent() {
+//         this.billingStatus = "WAITING_PAYMENT";
+//         this.invoiceSendDateTime = LocalDateTime.now();
+//     }
 
-    public String getInvoiceName() {
-        String year = Integer.toString(billingDate.getYear());
-        String month = Integer.toString(billingDate.getMonthValue());
-        return "%s년 %s월 청구서".formatted(year, month);
-    }
-}
+//     public String getInvoiceName() {
+//         String year = Integer.toString(billingDate.getYear());
+//         String month = Integer.toString(billingDate.getMonthValue());
+//         return "%s년 %s월 청구서".formatted(year, month);
+//     }
+// }
