b0ed0f8c6305347d94958d159bb105e829051e7f
김상훈 <rlatkdgns042@naver.com>
2024-07-31 15:04:02 +0900

    [KAN-229] cicd(-): BE 배포

--- server-messaging/src/main/java/kr/or/kosa/cmsplusmessaging/test/InfraTestController.java
+++ server-messaging/src/main/java/kr/or/kosa/cmsplusmessaging/test/InfraTestController.java
@@ -8,7 +8,7 @@ public class InfraTestController {
 
     @GetMapping("/infra-test-msg")
     public String infraTest() {
-        return "infra-test v0731-1";
+        return "infra-test v0731-2";
     }
     
 }
