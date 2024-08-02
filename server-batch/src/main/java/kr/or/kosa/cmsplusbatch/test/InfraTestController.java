30415010afa8f9f4337b7f4129e6732eaa065a3c
김상훈 <rlatkdgns042@naver.com>
2024-08-02 19:08:42 +0900

    [-] feat(batch): 배치 서버 배포

--- server-batch/src/main/java/kr/or/kosa/cmsplusbatch/test/InfraTestController.java
+++ server-batch/src/main/java/kr/or/kosa/cmsplusbatch/test/InfraTestController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class InfraTestController {
 
     @GetMapping("/infra-test")
