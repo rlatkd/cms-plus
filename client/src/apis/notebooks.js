b86634745977b233a695467c377cdea84efabc7b
김상훈 <rlatkdgns042@naver.com>
2024-08-03 19:24:50 +0900

    [-] feat(analysis): 통계서버 배포

--- client/src/apis/notebooks.js
+++ client/src/apis/notebooks.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
+const BASE_URL = import.meta.env.VITE_ANALYSIS_URL;
+
+axios.defaults.withCredentials = true;
+
 export const notebookAxios = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
